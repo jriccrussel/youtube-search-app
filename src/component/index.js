b7429c844fcf9378261c57cdc472eabc5cedbc9c
jrussel <jriccrussel@gmail.com>
2022-08-09 21:09:16 +0800

    add & update state, fetched dataug e set ang onTermSubmit with this.setState sa sulod and add video list pass ang state data

--- src/component/index.js
+++ src/component/index.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react'
 import SearchBar from './search'
 import youtube from '../api/youtube'
+import VideoList from './vidlist'
 
 class Index extends Component {
+    state = { videos: [] }
 
-    // term - parameter - 
-    onTermSubmit = (term) =>{
-        youtube.get('/search', {
+    // term - parameter - pasa nato ddto sa onFormSubmit function sulod nya 'props.onFormSubmit(this.state.term)' therefore ang 'term' is equal to 'this.state.term' ato g pasa
+    onTermSubmit = async (term) => {
+        const res = await youtube.get('/search', {
             params: {
                 q: term
             }
         })
 
         // console.log(term)
+        this.setState({ videos: res.data.items })
     }
 
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                {/* I have {this.state.videos.length} videos. */}
+                <VideoList videos={this.state.videos} />
             </div>
         )
     }
